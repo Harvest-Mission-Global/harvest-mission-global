c8ad45260b1326f3db62ed52e6bc2f62d0c2e7d2
kuritsuan <133067409+kuritsuan@users.noreply.github.com>
2026-03-16 21:51:03 +0800

    revert

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import backgroundImage from './images/background.jpg';
 function App() {
   return (
     <div style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <h1>Header Placeholder</h1>
+      {/* <h1>Header Placeholder</h1> */}
     </div>
   );
 }
