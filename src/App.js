bc600c89f1632f20a171c3a3bd442f2330e0dac2
kuritsuan <133067409+kuritsuan@users.noreply.github.com>
2026-03-16 22:55:52 +0800

    change size

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import backgroundImage from './images/background.jpg';
 function App() {
   return (
     <div style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', color: 'white' }}>
-      <div style={{ fontFamily: 'DM Sans', fontSize: '2rem',  color:"black", fontStyle: 'italic' }}>Welcome to</div>
-      <div style={{ fontFamily: 'DM Sans', fontSize: '3rem', fontWeight: 'bold', marginBottom: '2rem', color:"black" }}>Harvest Mission Global</div>
+      <div style={{ fontFamily: 'DM Sans', fontSize: '2.5rem',  color:"black", fontStyle: 'italic' }}>Welcome to</div>
+      <div style={{ fontFamily: 'DM Sans', fontSize: '3rem', fontWeight: 'bold', marginBottom: '2rem', color:"black" }}>HARVEST MISSION GLOBAL</div>
       <div>
         <a href="https://www.atx.hmccglobal.org/" style={{ margin: '0 1rem', padding: '0.5rem 1rem', fontSize: '1.2rem', backgroundColor: '#c3d2ff', border: 'none', borderRadius: '20px', textDecoration: 'none', color: 'black', boxShadow: '0 2px 4px rgba(0,0,0,0.2)', display: 'inline-block' }}>Austin</a>
         <a href="https://hk.hmccglobal.org/" style={{ margin: '0 1rem', padding: '0.5rem 1rem', fontSize: '1.2rem', backgroundColor: '#c3d2ff', border: 'none', borderRadius: '20px', textDecoration: 'none', color: 'black', boxShadow: '0 2px 4px rgba(0,0,0,0.2)', display: 'inline-block' }}>Hong Kong</a>
